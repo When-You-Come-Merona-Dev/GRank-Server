78831ec53cc17bf1c2b23bbc6eddeb17b7444444
hanbin8269 <hanbin8269@gmail.com>
2021-06-24 16:07:17 +0900

    chore : Add members field to group

--- src/domain/entities/group.py
+++ src/domain/entities/group.py
@@ -3,6 +3,7 @@ class Group:
         self.id = id
         self.name = name
         self.category = category
+        self.members = set()
 
     def change_category(self, new_caregory: str):
         self.category = new_caregory
