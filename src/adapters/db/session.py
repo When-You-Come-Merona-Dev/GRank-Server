79a3cfb55d851b726cb5311a3212f4127b669343
hanbin8269 <hanbin8269@gmail.com>
2021-06-24 09:16:22 +0900

    chore : Delete BaseORM and Create metadata

--- src/adapters/db/session.py
+++ src/adapters/db/session.py
@@ -1,14 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 from src.config import config
-from sqlalchemy.ext.declarative import declarative_base
 
 
 engine = create_engine(config.DATABASE_URL)
 
 LocalSession = scoped_session(sessionmaker(bind=engine))
 
-BaseORM = declarative_base()
+metadata = MetaData()
 
 
 def get_session():
