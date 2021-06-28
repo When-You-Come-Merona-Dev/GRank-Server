5e8ca9e52d3be1a2d6c26e20095757da83871582
hanbin8269 <hanbin8269@gmail.com>
2021-06-28 16:15:15 +0900

    feat : Implement list_github_user method in repository

--- src/adapters/repositories/github_user.py
+++ src/adapters/repositories/github_user.py
@@ -1,7 +1,7 @@
-from typing import Union
-from src.services.interfaces.repositories.github_user import AbstractGithubUserRepository
-from src.domain.entities.github_user import GithubUser
+from typing import Union, List
 from sqlalchemy.orm import Session
+from src.domain.entities.github_user import GithubUser
+from src.services.interfaces.repositories.github_user import AbstractGithubUserRepository
 
 
 class GithubUserRepository(AbstractGithubUserRepository):
@@ -16,8 +16,25 @@ class GithubUserRepository(AbstractGithubUserRepository):
         github_user = self.session.query(GithubUser).filter(GithubUser.username == username).first()
         return github_user
 
-    def renew_one_commit_count(self):
-        pass
+    def list_github_user(
+        self,
+        filters: dict,
+        page: int,
+        per_page: int,
+        order_by_field: str,
+    ) -> List[GithubUser]:
+        order_by = self.order_by_parser(order_by_field)
+        github_users = (
+            self.session.query(GithubUser)
+            .filter_by(**filters)
+            .order_by(order_by)
+            .limit(per_page)
+            .offset(page * per_page)
+        )
+        return github_users
 
-    def renew_all_commit_count(self):
-        pass
+    def renew_commit_count(self, commit_count) -> None:
+        return None
+
+    def order_by_parser(self, order_by_field):
+        return None
