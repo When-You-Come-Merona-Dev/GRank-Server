6db9fc76401f19f07b101197e76e40828fd6d5b9
hanbin8269 <hanbin8269@gmail.com>
2021-06-28 16:14:39 +0900

    chore : Fix Constant config variable

--- src/adapters/db/session.py
+++ src/adapters/db/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
-from src.config import config
+from src.config import CONFIG
 
 
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(CONFIG.DATABASE_URL)
 
 LocalSession = scoped_session(sessionmaker(bind=engine))
 
