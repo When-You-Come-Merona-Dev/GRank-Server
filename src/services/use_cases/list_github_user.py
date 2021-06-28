2b3d58b34db28b26d4dcdc933ad34d3659296d7f
hanbin8269 <hanbin8269@gmail.com>
2021-06-28 19:25:20 +0900

    chore : order_by to order_by_field

--- src/services/use_cases/list_github_user.py
+++ src/services/use_cases/list_github_user.py
@@ -10,12 +10,11 @@ class GithubUserListUserCase:
         self.crawler = crawler
 
     def execute(self, input_dto: GithubUserListResponseDto) -> List[GithubUserListRequestDto]:
-        order_by_field = input_dto.order_by
         github_users = self.repo.list_github_user(
             filters=input_dto.filters,
             page=input_dto.page,
             per_page=input_dto.per_page,
-            order_by_field=order_by_field,
+            order_by_field=input_dto.order_by,
         )  # List[GithubUser]
 
         return [GithubUserListResponseDto(**github_user.to_dict()) for github_user in github_users]
