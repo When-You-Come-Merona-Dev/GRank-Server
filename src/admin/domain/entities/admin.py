21a82f14844e49b7d6af1eed21e066ef749bf59e
hanbin8269 <hanbin8269@gmail.com>
2021-06-29 14:38:39 +0900

    feat: Add to_dict method to Admin, AdminCertificationCode Entities

--- src/admin/domain/entities/admin.py
+++ src/admin/domain/entities/admin.py
@@ -6,9 +6,25 @@ class Admin:
         self.created_at = None
         self.updated_at = None
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+            "created_at": self.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+            "updated_at": self.updated_at.strftime("%Y-%m-%dT%H:%M:%S"),
+        }
+
 
 class AdminCertificationCode:
     def __init__(self, code: str):
         self.code = code
         self.created_at = None
         self.updated_at = None
+
+    def to_dict(self):
+        return {
+            "code": self.code,
+            "created_at": self.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+            "updated_at": self.updated_at.strftime("%Y-%m-%dT%H:%M:%S"),
+        }
