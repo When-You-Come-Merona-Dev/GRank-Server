eaac7b891aae9605f3bcacc34a6ebdb19cc710cc
hanbin8269 <hanbin8269@gmail.com>
2021-06-29 11:26:44 +0900

    feat : Add Admin, AdminSecretCode entity

--- src/admin/domain/entities/admin.py
+++ src/admin/domain/entities/admin.py
@@ -1,7 +1,14 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Admin:
-    username: str
-    password: str
+    def __init__(self, username: str, password: str):
+        self.id = None
+        self.username = username
+        self.password = password
+        self.created_at = None
+        self.updated_at = None
+
+
+class AdminSecretCode:
+    def __init__(self, code: str):
+        self.code = code
+        self.created_at = None
+        self.updated_at = None
