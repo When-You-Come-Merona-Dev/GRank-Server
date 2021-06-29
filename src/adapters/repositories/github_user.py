6a99ac0985e0bc2cd1dcc447390809bd81565e26
hanbin8269 <hanbin8269@gmail.com>
2021-06-29 10:04:46 +0900

    feat: Add transaction

--- src/adapters/repositories/github_user.py
+++ src/adapters/repositories/github_user.py
@@ -21,8 +21,12 @@ class GithubUserRepository(AbstractGithubUserRepository):
         self.session = session
 
     def create_github_user(self, github_user: GithubUser) -> None:
-        self.session.add(github_user)
-        self.session.commit()
+        try:
+            self.session.add(github_user)
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
 
     def get_github_user_by_username(self, username: str) -> Union[GithubUser, None]:
         github_user = self.session.query(GithubUser).filter(GithubUser.username == username).first()
