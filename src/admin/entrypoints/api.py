b0b84d71d4ce4aa18a3d58e64fd15d3ec9363dd6
hanbin8269 <hanbin8269@gmail.com>
2021-06-30 14:39:39 +0900

    feat: Add login_admin api

--- src/admin/entrypoints/api.py
+++ src/admin/entrypoints/api.py
@@ -1,14 +1,22 @@
+from sqlalchemy.orm.session import Session
+from starlette import status
+from fastapi import Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.param_functions import Depends
+from fastapi.routing import APIRouter
 from src.admin.services.use_cases.add_admin import AdminAddUserCase
+from src.admin.services.use_cases.login_admin import AdminLoginUserCase
 from src.admin.adapters.repository import AdminRepository
 from src.infra.db.session import get_session
-from fastapi.param_functions import Depends
-from sqlalchemy.orm.session import Session
-from src.admin.entrypoints.schema import AdminCreateRequestDto, AdminCreateResponseDto
-from fastapi.routing import APIRouter
-from starlette import status
-
+from src.admin.entrypoints.schema import (
+    AdminCreateRequestDto,
+    AdminCreateResponseDto,
+    AdminLoginRequestDto,
+    AdminLoginResponseDto,
+)
 
 router = APIRouter()
+security = HTTPBearer()
 
 
 @router.post("/admin", response_model=AdminCreateResponseDto, status_code=status.HTTP_201_CREATED)
@@ -17,6 +25,17 @@ def add_admin(
 ) -> AdminCreateResponseDto:
     repo = AdminRepository(session)
     use_case = AdminAddUserCase(repo=repo)
+
+    output_dto = use_case.execute(admin)
+    return output_dto
+
+
+@router.post("/admin/login", response_model=AdminLoginResponseDto, status_code=status.HTTP_200_OK)
+def login_admin(
+    admin: AdminLoginRequestDto, session: Session = Depends(get_session)
+) -> AdminLoginResponseDto:
+    repo = AdminRepository(session)
+    use_case = AdminLoginUserCase(repo=repo)
 
     output_dto = use_case.execute(admin)
     return output_dto
