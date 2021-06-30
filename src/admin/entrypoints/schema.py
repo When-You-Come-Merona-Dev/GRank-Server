ea86217df7c3562fc173f8329b21ee177d17560c
hanbin8269 <hanbin8269@gmail.com>
2021-06-30 14:40:10 +0900

    feat: Add AdminLogin dtos

--- src/admin/entrypoints/schema.py
+++ src/admin/entrypoints/schema.py
@@ -7,6 +7,15 @@ class AdminCreateRequestDto(BaseModel):
     certification_code: str
 
 
+class AdminLoginRequestDto(BaseModel):
+    username: str
+    password: str
+
+
 # Response
 class AdminCreateResponseDto(BaseModel):
     username: str
+
+
+class AdminLoginResponseDto(BaseModel):
+    token: str
