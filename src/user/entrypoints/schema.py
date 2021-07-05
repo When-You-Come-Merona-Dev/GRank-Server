074d7d727f9705e342941d43f8a7f8d03a6c4d8d
hanbin8269 <hanbin8269@gmail.com>
2021-07-05 23:30:13 +0900

    chore : Update SNSGithubCallbackResponseDto field

--- src/user/entrypoints/schema.py
+++ src/user/entrypoints/schema.py
@@ -7,11 +7,4 @@ class SNSGithubCallbackRequestDto(BaseModel):
 
 # Response
 class SNSGithubCallbackResponseDto(BaseModel):
-    id: int
-    username: str
-    avatar_url: str
-    commit_count: int
-    is_approved: bool
-    groups: set
-    created_at: str
-    updated_at: str
+    token: str
