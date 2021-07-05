31cfa0f2d0f93e20288c7cd91d74307d5cec8da4
hanbin8269 <hanbin8269@gmail.com>
2021-07-05 23:30:29 +0900

    feat : Add to_dict method in User

--- src/user/domain/entities/user.py
+++ src/user/domain/entities/user.py
@@ -1,5 +1,16 @@
 class User:
-    def __init__(self, email: str, sns_service_id: str):
+    def __init__(self, github_id: str, password: str):
         self.id = None
-        self.email = email
-        self.sns_service_id = sns_service_id
+        self.github_id = github_id
+        self.password = password
+        self.created_at = None
+        self.updated_at = None
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "github_id": self.github_id,
+            "password": self.password,
+            "created_at": self.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+            "updated_at": self.updated_at.strftime("%Y-%m-%dT%H:%M:%S"),
+        }
