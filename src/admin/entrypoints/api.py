da69da757611c78267934a2365463a6d4a259a82
hanbin8269 <hanbin8269@gmail.com>
2021-07-06 19:49:12 +0900

    chore : Apply handlers

--- src/admin/entrypoints/api.py
+++ src/admin/entrypoints/api.py
@@ -1,13 +1,7 @@
-from sqlalchemy.orm.session import Session
 from starlette import status
-from fastapi import Security
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from fastapi.param_functions import Depends
 from fastapi.routing import APIRouter
-from src.admin.services.use_cases.add_admin import AdminAddUserCase
-from src.admin.services.use_cases.login_admin import AdminLoginUserCase
-from src.admin.adapters.repository import AdminRepository
-from src.infra.db.session import get_session
+from src.admin.services.unit_of_work import SQLAlchemyUnitOfWork
+from src.admin.services import handlers
 from src.admin.entrypoints.schema import (
     AdminCreateRequestDto,
     AdminCreateResponseDto,
@@ -16,26 +10,13 @@ from src.admin.entrypoints.schema import (
 )
 
 router = APIRouter()
-security = HTTPBearer()
 
 
 @router.post("/admin", response_model=AdminCreateResponseDto, status_code=status.HTTP_201_CREATED)
-def add_admin(
-    admin: AdminCreateRequestDto, session: Session = Depends(get_session)
-) -> AdminCreateResponseDto:
-    repo = AdminRepository(session)
-    use_case = AdminAddUserCase(repo=repo)
-
-    output_dto = use_case.execute(admin)
-    return output_dto
+def add_admin(input_dto: AdminCreateRequestDto) -> AdminCreateResponseDto:
+    return handlers.add_admin(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
 
 
 @router.post("/admin/login", response_model=AdminLoginResponseDto, status_code=status.HTTP_200_OK)
-def login_admin(
-    admin: AdminLoginRequestDto, session: Session = Depends(get_session)
-) -> AdminLoginResponseDto:
-    repo = AdminRepository(session)
-    use_case = AdminLoginUserCase(repo=repo)
-
-    output_dto = use_case.execute(admin)
-    return output_dto
+def login_admin(input_dto: AdminLoginRequestDto) -> AdminLoginResponseDto:
+    return handlers.admin_login(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
