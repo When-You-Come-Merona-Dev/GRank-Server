2c585928c173ca65862151e00988fa0d3877da92
hanbin8269 <hanbin8269@gmail.com>
2021-07-06 16:07:30 +0900

    feat: Add make_public_github_user api

--- src/github_user/entrypoints/api.py
+++ src/github_user/entrypoints/api.py
@@ -7,12 +7,12 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from src.infra.db.session import get_session
 from src.github_user.entrypoints.parsers.query_parser import QueryParameterParser
 from src.github_user.entrypoints.schema import (
-    GithubUserCreateRequestDto,
-    GithubUserCreateResponseDto,
     GithubUserListResponseDto,
     GithubUserListRequestDto,
     GithubUserApproveResponseDto,
     GithubUserApproveRequestDto,
+    GithubUserMakePublicResponseDto,
+    GithubUserMakePublicRequestDto,
     GithubUserRenewAllRequestDto,
     GithubUserRenewAllResponseDto,
     GithubUserRenewOneRequestDto,
@@ -22,35 +22,18 @@ from src.github_user.entrypoints.schema import (
 )
 from src.github_user.adapters.external_api import RequestExternalAPIClient
 from src.github_user.adapters.repository import GithubUserRepository
-from src.github_user.services.use_cases.add_github_user import GithubUserAddUseCase
 from src.github_user.services.use_cases.list_github_user import GithubUserListUserCase
 from src.github_user.services.use_cases.approve_github_user import GithubUserApproveUseCase
 from src.github_user.services.use_cases.renew_one_github_user import GithubUserRenewOneUseCase
 from src.github_user.services.use_cases.renew_all_github_user import GithubUserRenewAllUseCase
+from src.github_user.services.use_cases.make_public_github_user import GithubUserMakePublicUseCase
 from src.github_user.services.use_cases.sns_github_login import SNSGithubLoginUseCase
-from src.utils.permissions import IsAdmin, check_permissions
+from src.utils.permissions import IsAdmin, IsOwnerOrAdmin, check_permissions
 from src.config import CONFIG
 
 router = APIRouter()
 security = HTTPBearer()
 # TODO : api 구조화
-
-
-@router.post(
-    "/github-user",
-    response_model=GithubUserCreateResponseDto,
-    status_code=status.HTTP_201_CREATED,
-)
-def add_github_user(
-    user: GithubUserCreateRequestDto, session: Session = Depends(get_session)
-) -> GithubUserCreateResponseDto:
-    crawler = RequestExternalAPIClient()
-    repo = GithubUserRepository(session)
-
-    use_case = GithubUserAddUseCase(repo=repo, crawler=crawler)
-
-    output_dto = use_case.execute(user)
-    return output_dto
 
 
 @router.get(
@@ -68,10 +51,9 @@ def list_github_user(
     page, per_page = parser.parse_pagination_parameter()
     order_by = parser.parse_order_by_rule_parameter()
 
-    crawler = None
     repo = GithubUserRepository(session)
 
-    use_case = GithubUserListUserCase(repo=repo, crawler=crawler)
+    use_case = GithubUserListUserCase(repo=repo)
 
     request_dto = GithubUserListRequestDto(
         filters={}, page=page, per_page=per_page, order_by=order_by
@@ -80,6 +62,27 @@ def list_github_user(
     response_dto = use_case.execute(request_dto)
 
     return response_dto
+
+
+@router.patch(
+    "/github-user/{username}/make-public",
+    response_model=GithubUserMakePublicResponseDto,
+    status_code=status.HTTP_200_OK,
+)
+def make_public_github_user(
+    request: Request,
+    username: str,
+    session: Session = Depends(get_session),
+    credentials: HTTPAuthorizationCredentials = Security(security),
+) -> GithubUserMakePublicResponseDto:
+    check_permissions(request=request, permissions=[IsOwnerOrAdmin])
+
+    repo = GithubUserRepository(session)
+    use_case = GithubUserMakePublicUseCase(repo=repo)
+    input_dto = GithubUserMakePublicRequestDto(username=username)
+
+    output_dto = use_case.execute(input_dto)
+    return output_dto
 
 
 @router.patch(
@@ -108,15 +111,19 @@ def approve_github_user(
     status_code=status.HTTP_200_OK,
 )
 def renew_one_github_user(
+    request: Request,
     username: str,
     session: Session = Depends(get_session),
+    credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> GithubUserRenewOneResponseDto:
+    check_permissions(request=request, permissions=[IsOwnerOrAdmin])
+
     repo = GithubUserRepository(session)
-    crawler = RequestExternalAPIClient()
+    external_api = RequestExternalAPIClient()
 
     input_dto = GithubUserRenewOneRequestDto(username=username)
 
-    use_case = GithubUserRenewOneUseCase(repo=repo, crawler=crawler)
+    use_case = GithubUserRenewOneUseCase(repo=repo, external_api=external_api)
 
     output_dto = use_case.execute(input_dto)
     return output_dto
