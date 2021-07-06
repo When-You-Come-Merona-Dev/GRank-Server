f03264589f41bedb22bba18f8b97ea4946f7619c
hanbin8269 <hanbin8269@gmail.com>
2021-07-06 12:22:57 +0900

    chore : Move SNSGithubLoginUseCase

--- src/github_user/services/use_cases/sns_github_login.py
+++ src/github_user/services/use_cases/sns_github_login.py
@@ -1,14 +1,17 @@
 from fastapi.exceptions import HTTPException
 from src.github_user.services.interfaces.external_api import AbstractExternalAPIClient
-from src.user.entrypoints.schema import SNSGithubCallbackRequestDto, SNSGithubCallbackResponseDto
-from src.user.services.interfaces.repository import AbstractUserRepository
-from src.user.domain.entities.user import User
+from src.github_user.entrypoints.schema import (
+    SNSGithubCallbackRequestDto,
+    SNSGithubCallbackResponseDto,
+)
+from src.github_user.services.interfaces.repository import AbstractGithubUserRepository
+from src.github_user.domain.entities.github_user import GithubUser
 from src.utils.hasher import hash_password, check_password
 from src.utils.token_handlers import jwt_encode_handler, jwt_payload_handler
 
 
 class SNSGithubLoginUseCase:
-    def __init__(self, repo: AbstractUserRepository, external_api: AbstractExternalAPIClient):
+    def __init__(self, repo: AbstractGithubUserRepository, external_api: AbstractExternalAPIClient):
         self.repo = repo
         self.external_api = external_api
 
@@ -26,8 +29,10 @@ class SNSGithubLoginUseCase:
                 raise HTTPException(status_code=400, detail="Can't login with recieved credentials")
         else:
             hashed_password = hash_password(user_info["node_id"])
-            user_instance = User(github_id=github_id, password=hashed_password)
-            self.repo.create_user(user=user_instance)
+            user_instance = GithubUser(
+                github_id=github_id, password=hashed_password
+            )  # avatar url, username 정보 이후 더 추가
+            self.repo.create_github_user(github_user=user_instance)
 
         payload = jwt_payload_handler(user_instance)
         token = jwt_encode_handler(payload)
