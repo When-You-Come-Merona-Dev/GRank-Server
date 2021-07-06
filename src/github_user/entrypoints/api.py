3416ea579dce11904f6aa46fa78808f841e72632
hanbin8269 <hanbin8269@gmail.com>
2021-07-06 19:33:37 +0900

    feat: Change UseCase to handlers

--- src/github_user/entrypoints/api.py
+++ src/github_user/entrypoints/api.py
@@ -1,10 +1,8 @@
 from typing import Optional, List
 from fastapi import security
 from starlette import status
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, Request, Security
+from fastapi import APIRouter, Request, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from src.infra.db.session import get_session
 from src.github_user.entrypoints.parsers.query_parser import QueryParameterParser
 from src.github_user.entrypoints.schema import (
     GithubUserListResponseDto,
@@ -20,20 +18,13 @@ from src.github_user.entrypoints.schema import (
     SNSGithubCallbackRequestDto,
     SNSGithubCallbackResponseDto,
 )
-from src.github_user.adapters.external_api import RequestExternalAPIClient
-from src.github_user.adapters.repository import GithubUserRepository
-from src.github_user.services.use_cases.list_github_user import GithubUserListUserCase
-from src.github_user.services.use_cases.approve_github_user import GithubUserApproveUseCase
-from src.github_user.services.use_cases.renew_one_github_user import GithubUserRenewOneUseCase
-from src.github_user.services.use_cases.renew_all_github_user import GithubUserRenewAllUseCase
-from src.github_user.services.use_cases.make_public_github_user import GithubUserMakePublicUseCase
-from src.github_user.services.use_cases.sns_github_login import SNSGithubLoginUseCase
+from src.github_user.services.unit_of_work import SQLAlchemyUnitOfWork
+from src.github_user.services import handlers, readers
 from src.utils.permissions import IsAdmin, IsOwnerOrAdmin, check_permissions
 from src.config import CONFIG
 
 router = APIRouter()
 security = HTTPBearer()
-# TODO : api 구조화
 
 
 @router.get(
@@ -44,24 +35,17 @@ def list_github_user(
     page: Optional[int] = None,
     per_page: Optional[int] = None,
     order_by: Optional[str] = "commit_count",
-    session: Session = Depends(get_session),
 ) -> List[GithubUserListResponseDto]:
 
     parser = QueryParameterParser(query=request.query_params)
     page, per_page = parser.parse_pagination_parameter()
     order_by = parser.parse_order_by_rule_parameter()
 
-    repo = GithubUserRepository(session)
-
-    use_case = GithubUserListUserCase(repo=repo)
-
-    request_dto = GithubUserListRequestDto(
+    input_dto = GithubUserListRequestDto(
         filters={}, page=page, per_page=per_page, order_by=order_by
     )
 
-    response_dto = use_case.execute(request_dto)
-
-    return response_dto
+    return readers.list_github_user(input_dto=input_dto, uow=SQLAlchemyUnitOfWork())
 
 
 @router.patch(
@@ -72,17 +56,13 @@ def list_github_user(
 def make_public_github_user(
     request: Request,
     username: str,
-    session: Session = Depends(get_session),
     credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> GithubUserMakePublicResponseDto:
     check_permissions(request=request, permissions=[IsOwnerOrAdmin])
 
-    repo = GithubUserRepository(session)
-    use_case = GithubUserMakePublicUseCase(repo=repo)
     input_dto = GithubUserMakePublicRequestDto(username=username)
 
-    output_dto = use_case.execute(input_dto)
-    return output_dto
+    return handlers.make_public_github_user(input_dto, SQLAlchemyUnitOfWork)
 
 
 @router.patch(
@@ -93,16 +73,13 @@ def make_public_github_user(
 def approve_github_user(
     request: Request,
     username: str,
-    session: Session = Depends(get_session),
     credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> GithubUserApproveResponseDto:
     check_permissions(request=request, permissions=[IsAdmin])
 
-    repo = GithubUserRepository(session)
-    use_case = GithubUserApproveUseCase(repo=repo)
     input_dto = GithubUserApproveRequestDto(username=username)
-    output_dto = use_case.execute(input_dto)
-    return output_dto
+
+    return handlers.approve_github_user(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
 
 
 @router.patch(
@@ -113,20 +90,13 @@ def approve_github_user(
 def renew_one_github_user(
     request: Request,
     username: str,
-    session: Session = Depends(get_session),
     credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> GithubUserRenewOneResponseDto:
     check_permissions(request=request, permissions=[IsOwnerOrAdmin])
 
-    repo = GithubUserRepository(session)
-    external_api = RequestExternalAPIClient()
-
     input_dto = GithubUserRenewOneRequestDto(username=username)
 
-    use_case = GithubUserRenewOneUseCase(repo=repo, external_api=external_api)
-
-    output_dto = use_case.execute(input_dto)
-    return output_dto
+    return handlers.renew_one_github_user(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
 
 
 @router.patch(
@@ -136,20 +106,13 @@ def renew_one_github_user(
 )
 def renew_all_github_user(
     request: Request,
-    session: Session = Depends(get_session),
     credentials: HTTPAuthorizationCredentials = Security(security),
 ) -> List[GithubUserRenewAllResponseDto]:
     check_permissions(request=request, permissions=[IsAdmin])
 
-    repo = GithubUserRepository(session)
-    crawler = RequestExternalAPIClient()
-
     input_dto = GithubUserRenewAllRequestDto()
 
-    use_case = GithubUserRenewAllUseCase(repo=repo, crawler=crawler)
-
-    output_dto = use_case.execute(input_dto)
-    return output_dto
+    return handlers.renew_all_github_user(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
 
 
 @router.get("/sns/github")
@@ -164,13 +127,7 @@ def get_github_login_url():
 @router.get("/sns/github/callback")
 def github_callback(
     code: str,
-    session: Session = Depends(get_session),
 ) -> SNSGithubCallbackResponseDto:
-    repo = GithubUserRepository(session)
-    external_api = RequestExternalAPIClient()
     input_dto = SNSGithubCallbackRequestDto(code=code)
 
-    use_case = SNSGithubLoginUseCase(repo=repo, external_api=external_api)
-
-    output_dto = use_case.execute(input_dto)
-    return output_dto
+    return handlers.github_callback(input_dto=input_dto, uow=SQLAlchemyUnitOfWork)
