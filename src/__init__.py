34f5c1222d332a4f646bb86466e8024767cc3878
hanbin8269 <hanbin8269@gmail.com>
2021-07-09 15:40:41 +0900

    feat: Add exception middleware

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,12 @@
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from src.infra.db.mapper import start_mappers
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from src.infra.db.mapper import start_mappers
 from src.middlewares.authentication import AuthenticationMiddleware, JWTAuthenticationBackend
 from src.config import CONFIG
+from src.utils.exceptions import APIException
 
 
 def init_middleware(app: FastAPI):
@@ -17,6 +20,14 @@ def init_middleware(app: FastAPI):
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    @app.exception_handler(APIException)
+    async def api_exception_handler(request: Request, ex: APIException):
+        return JSONResponse(status_code=ex.status_code, content={"error": ex.detail})
+
+    @app.exception_handler(Exception)
+    async def internal_exception_handler(request: Request, ex: Exception):
+        return JSONResponse(status_code=500, content={"error": str(ex)})
 
 
 def init_orm():
