c4ce993b972ab034d05e05c926a9d9da59d3b917
hanbin8269 <hanbin8269@gmail.com>
2021-08-01 01:35:01 +0900

    chore : Fix response namespace error

--- src/middlewares/api_logger.py
+++ src/middlewares/api_logger.py
@@ -1,8 +1,9 @@
 import time
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.responses import Response
+from starlette.responses import JSONResponse, Response
 from fastapi import Request
 from src.utils.logger import make_log
+from src.utils.exceptions import APIException
 
 
 class APILoggerMiddleware(BaseHTTPMiddleware):
@@ -19,7 +20,16 @@ class APILoggerMiddleware(BaseHTTPMiddleware):
             response = await call_next(request)
             await make_log(request=request, response=response)
         except Exception as e:
-            error = e
+            error = await exception_handler(e)
+            error_dict = dict(status=error.status_code, detail=error.detail, code=error.code)
+            response = JSONResponse(status_code=error.status_code, content=error_dict)
             await make_log(request=request, error=error)
 
         return response
+
+
+async def exception_handler(error: Exception):
+    if not isinstance(error, APIException):
+        error = APIException(ex=error, detail=str(error))
+
+    return error
